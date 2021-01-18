83164957ae9fbbf50e9ecb55c41bebefbd71536c
daniel <danielkaczmarczyk2@gmail.com>
2021-01-18 11:20:43 -0800

    first solve wordCount kr

---
+++ bradfield/prep/c/kr/1.5/wordCount.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/* program that counts lines, words, and characters in input.
+ * a word is a sequence of characters that do not contain
+ * a blank, a tab, or newline. it is a bare-bones version of
+ * wc program.
+ */
+
+int main(void) {
+
+  #define IN 1   /* inside a word */
+  #define OUT 0  /* outdside a word */
+
+  int c, chars, lines, state, wc;
+
+  chars = 0;
+  lines = 0;
+  wc    = 0;     
+  state = OUT;
+
+  while ((c = getchar()) != EOF) {
+    chars++;
+    if (c == '\n' || c == '\t' || c == ' ') {
+      if (c == '\n') ++lines;
+      if (state == IN) wc++;
+      state = OUT;
+    } else {
+      state = IN;
+    }
+  }
+  
+  printf("\nTotals:\n");
+  printf("Lines: %d\n", lines);
+  printf("Chars: %d\n", chars);
+  printf("Words: %d\n", wc);
+  return 0;
+}
