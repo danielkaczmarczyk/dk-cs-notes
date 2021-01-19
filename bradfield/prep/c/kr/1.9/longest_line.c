6495e070fa5c3055d85ddbdf58672a6370d9dcfc
daniel <danielkaczmarczyk2@gmail.com>
2021-01-18 19:08:30 -0800

    getline copied

--- bradfield/prep/c/kr/1.9/longest_line.c
+++ bradfield/prep/c/kr/1.9/longest_line.c
@@ -1,4 +1,56 @@
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line length */
 
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
 
+/* print the longest input line */
+int main(void) {
+  int len;                 // current line length
+  int max;                 // max line length so far
+  char line[MAXLINE];      // current input line
+  char longest[MAXLINE];   // longest input line saved here
+
+  max = 0;
+  while ((len = getline(line, MAXLINE)) > 0) {
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    } 
+  }
+
+  if (max > 0) {
+    printf("%s", longest);
+    return 0;
+  }
+  return 1;
+}
+
+int getline(char s[], int lim) {
+  int c, i;
+
+  for (i = 0; i < lim - 1 && (c=getchar()) != EOF && c != '\n'; i++) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i] = c;
+    i++;
+  }
+
+  s[i] = '\0';
+  return i;
+}
+
+void copy(char to[], char from[]) {
+  int i;
+  i = 0;
+  while ((to[i] = from[i]) != '\0') {
+    ++i;
+  }
+}
+
+
+
+
+}
