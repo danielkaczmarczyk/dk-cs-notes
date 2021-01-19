256156d50689c76a160eee3b6ee2aac2f822e1a6
daniel <danielkaczmarczyk2@gmail.com>
2021-01-18 20:12:20 -0800

    making progress, finally found the bug

--- bradfield/prep/c/kr/1.9/my_longest_line.c
+++ bradfield/prep/c/kr/1.9/my_longest_line.c
@@ -15,12 +15,19 @@ int collectline(char line[], int max_length);
 int main(void) {
 
   char line[MAX_LINE_LENGTH];
-  char line_length = 0;
+  int line_length = 0;
 
-  line_length = collectline(line, MAX_LINE_LENGTH);
+  char longest_line[MAX_LINE_LENGTH];
+  int longest_line_length = 0;
+
+  while ((line_length = collectline(line, MAX_LINE_LENGTH)) > 0) {
+    if (line_length > longest_line_length) {
+      longest_line_length = line_length;
+    }
+  }
 
   printf("your line: %s\n", line);
-  printf("line length: %d\n", line_length);
+  printf("longest line length: %d\n", longest_line_length);
   return 0; 
 }
 
@@ -29,15 +36,18 @@ int collectline(char line[], int max_length) {
   int c, i;
 
   for (i = 0; i < max_length - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    printf("in for loop, currently placing %c at index %d\n", c, i);
     line[i] = c;
   }
 
   if (c == '\n') {
     line[i] = c;
     i++;
+    printf("%d value of i\n", i);
+    if (i > 2) {
+      line[i] = '\0';
+    }
   }
-
-  line[i] = '\0';
 
   return i;
 }
