2bed1cbb34d880f74c830bdf72fb953e15a58269
daniel <danielkaczmarczyk2@gmail.com>
2021-01-19 14:45:49 -0800

    init comment remove

---
+++ bradfield/prep/c/kr/1.10/exercises/1-23.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#define ARRAY_SIZE 1000
+#define MAX_LINE_LEN 10
+
+/*
+ * program that removes comments from a C program source
+ *
+ * TODO
+ * [ ] handle // comments
+ * [ ] handle multiline comments
+ */
+
+void break_line(char source[], int source_len);
+int collect_line(char target[]);
+void print_invisibles(char target[]); 
+
+int main(void) {
+  char line[ARRAY_SIZE];
+  int line_length = 0;
+
+  while (1) {
+    line_length = collect_line(line);
+    print_invisibles(line);
+  }
+}
+
+/* 
+ * A function that collects a line of input from the stdin, copies it to
+ * target char array, and returns the line length.
+ */
+int collect_line(char target[]) {
+  int c;
+  int i = 0;
+  printf(">>> ");
+  while((c = getchar()) != '\n') {
+    target[i] = c; 
+    i++;
+  }
+
+  if (c == '\n') {
+    target[i] = '\n';
+    i++; 
+  }
+
+  target[i] = '\0';
+  return i; 
+}
+
+/* prints invisible characters (tabs and spaces) for debugging */
+void print_invisibles(char target[]) {
+  int i = 0;
+  char c;
+  printf("DEBUG: ");
+  while((c = target[i]) != '\0') {
+    if (c == ' ') {
+      printf("\\s");
+    } else if (c == '\t') {
+      printf("\\t");
+    } else {
+      putchar(c);
+    }
+    i++;
+  }
+}
+
