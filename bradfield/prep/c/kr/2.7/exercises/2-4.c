ea96819ac4084924b18ba7f30eaf30585133f579
daniel <danielkaczmarczyk2@gmail.com>
2021-01-20 20:38:26 -0800

    sleeping on the problem helped. got the boolean other way around

---
+++ bradfield/prep/c/kr/2.7/exercises/2-4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/* squeeze alt - deletes each char in s1 that can be found in s2 */
+
+void squeeze(char s1[], char s2[]);
+
+int main(void) {
+  char s1[6];
+  char s2[5];
+
+  s1[0] = 'd';
+  s1[1] = 'u';
+  s1[2] = 'm';
+  s1[3] = 'b';
+  s1[4] = 'o';
+  s1[5] = '\0';
+
+  s2[0] = 'b';
+  s2[1] = 'o';
+  s2[2] = 'd';
+  s2[3] = 'y';
+  s2[4] = '\0';
+
+  squeeze(s1, s2);
+
+  printf("%s\n", s1);
+  printf("um\n");
+
+}
+
+void squeeze(char s1[], char s2[]) {
+  int c, i, j;
+  int delete;
+
+  for (i = j = 0; s1[i] != '\0'; i++) {
+    delete = 0;
+    c = s1[i];
+    printf("out: i:%d j:%d del:%d c:%c\n", i, j, delete, c);
+    for (int m = 0; s2[m] != '\0'; m++) {
+      printf("in: m:%d s2[m]:%c, c:%c\n", m, s2[m], c);
+      if (c == s2[m]) {
+        printf("deleting %c!\n", c);
+        delete = 1;
+        break;
+      }
+    }
+
+    if (!delete) {
+      s1[j++] = s1[i];
+    }
+  }
+  s1[j] = '\0';
+}
+
