5cbaf6c12ab319ed576f6bc765774a11296a114f
daniel <danielkaczmarczyk2@gmail.com>
2021-01-22 18:21:24 -0800

    starting on 2-8

---
+++ bradfield/prep/c/kr/2.9/exercises/2-8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int rightrot(unsigned x, int n);
+
+int main(void) {
+  // 
+}
+
+
+int rightrot(unsigned x, int n) {
+  int lsb;
+
+  while (n-- > 0) {
+    printf("%d\n", n);
+  }
+}
