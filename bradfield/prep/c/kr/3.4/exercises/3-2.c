050a82ec7728065e9ecb01e8ca32acb718710ebc
daniel <danielkaczmarczyk2@gmail.com>
2021-01-24 18:28:33 -0800

    unescape works just fine, needs a few tweaks in terms of style

--- bradfield/prep/c/kr/3.4/exercises/3-2.c
+++ bradfield/prep/c/kr/3.4/exercises/3-2.c
@@ -2,12 +2,19 @@
 #define ARRAY_SIZE 1000
 
 void escape(char source[], char target[]);
+void unescape(char source[], char target[]);
 
 int main(void) {
   char source[] = "Source\nString\ttest\n";
   char target[ARRAY_SIZE];
+  char target_2[ARRAY_SIZE];
+
+  printf("\nPre:\n%s", source);
   escape(source, target);
-  printf("%s\n", target);
+  printf("\nPost:\n%s\n\n", target);
+  unescape(target, target_2);
+  printf("\nReverted:\n%s\n", target_2);
+ 
 }
 
 void escape(char source[], char target[]) {
@@ -33,4 +40,41 @@ void escape(char source[], char target[]) {
     i++;
   }
   target[j] = '\0';
+}
+
+void unescape(char source[], char target[]) {
+  int i = 0;
+  int j = 0;
+  char c, c2;
+  
+  while ((c = source[i]) != '\0') {
+    printf("encountered %c: %x. ", c, c);
+    printf("i: %d ", i);
+    switch (c) {
+      case '\\' :
+        c2 = source[i + 1];
+        switch (c2) {
+          case 't':
+            printf("inputting a tab at position %d\n", j);
+            target[j] = '\t';
+            j++;
+            break;
+          case 'n':
+            printf("inputting a newline at position %d\n", j);
+            target[j] = '\n';
+            j++;
+            break;
+        }
+        i += 2;
+        break;
+      default:
+        printf("inputting a char at position %d\n", j);
+        target[j] = source[i];
+        j++;
+        i++;
+    }
+  }
+  printf("adding null byte at position %d \n", i);
+  printf("i: %d\n", i);
+  target[i] = '\0';
 }
