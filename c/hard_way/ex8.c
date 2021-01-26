feb6377ae3a2c53066bc4c386720e31a49b794ea
daniel <danielkaczmarczyk2@gmail.com>
2021-01-26 15:30:31 -0800

    modified the code to not include the initial argument with the filename

--- c/hard_way/ex8.c
+++ c/hard_way/ex8.c
@@ -3,9 +3,9 @@
 int main(int argc, char *argv[]) {
   int i = 0;
   
-  if (argc == 1) {
+  if (argc == 2) {
     printf("You only have one argument. You suck\n");
-  } else if (argc < 1 && argc < 4) {
+  } else if (argc > 2 && argc < 5) {
     printf("Here are your arguments: \n");
 
     for (i = 0; i < argc; i++) {
