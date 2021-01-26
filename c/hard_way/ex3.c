8008b0ef7c48157d9d0117462e62083b8f5566b5
daniel <danielkaczmarczyk2@gmail.com>
2021-01-26 15:20:37 -0800

    messing with the pointers

--- c/hard_way/ex3.c
+++ c/hard_way/ex3.c
@@ -4,8 +4,10 @@ int main()
 {
     int age = 10;
     int height = 72;
+    char name[] = "Daniel";
 
     printf("I am %d years old. ", age);
     printf("I am %d inches tall.\n", height);
+    printf("My name is %s\n", name + 11);      // moving the pointer
 }
   
