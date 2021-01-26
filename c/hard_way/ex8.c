5e07b37372645fd570e942b21c88718ce3f23702
daniel <danielkaczmarczyk2@gmail.com>
2021-01-26 15:31:56 -0800

    fixed an error in counting

--- c/hard_way/ex8.c
+++ c/hard_way/ex8.c
@@ -3,8 +3,8 @@
 int main(int argc, char *argv[]) {
   int i = 0;
   
-  if (argc == 2) {
-    printf("You only have one argument. You suck\n");
+  if (argc == 1) {
+    printf("You have not passed any arguments. You may suck\n.");
   } else if (argc > 2 && argc < 5) {
     printf("Here are your arguments: \n");
 
