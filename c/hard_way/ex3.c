000be5e7514442e66b798b94102c034298be72cc
daniel <danielkaczmarczyk2@gmail.com>
2021-01-26 13:54:12 -0800

    cleaned up the exercises folder

---
+++ c/hard_way/ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+
+    printf("I am %d years old. ", age);
+    printf("I am %d inches tall.\n", height);
+}
+  
--- c/hard_way/exercise_2/ex1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-  int distance = 100;
-  printf("You are %d miles away\n", distance);
-  return 0;
-}
-
