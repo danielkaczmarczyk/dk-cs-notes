0717b594ee8a7ed69f8bf18fe982f36bc80fbc1a
daniel <danielkaczmarczyk2@gmail.com>
2021-01-27 13:47:07 -0800

    broke the pointer code

--- c/hard_way/ex15.c
+++ c/hard_way/ex15.c
@@ -12,8 +12,24 @@ int main(int argc, char *argv[]) {
   int count = sizeof(ages) / sizeof(int);
   int i = 0;
 
+  // printing
+  // first way using indexing
   for (i = 0; i < count; i++) {
     printf("%s has %d years alive\n", names[i], ages[i]);
   }
+
+  printf("--->\n");
+
+  // setup the pointers to the start of arrays;
+  int *cur_age = ages;      // pointer to the start of array ages.
+  char **cur_name = names;  // pointer to pointers to strings (**)
+
+  // printing second way using pointers
+  for (i = 0; i < count; i++) {
+    printf("%s is %d years old\n", *cur_name, *cur_age);
+    cur_name += i;
+    cur_age += i;
+  }
+
 
 }
