18a3abc765eddef748675088819f8d4a0e9c23d0
daniel <danielkaczmarczyk2@gmail.com>
2021-01-26 16:18:08 -0800

    ex10 typed in

--- c/hard_way/ex10.c
+++ c/hard_way/ex10.c
@@ -7,5 +7,48 @@ int main(int argc, char *argv[]) {
     // abort the program
     return 1;
   }
+
+  int i = 0;
+  for (i = 0; argv[1][i] != '\0'; i++) {
+    char letter = argv[1][i];
+
+    switch (letter) {
+      case 'a':
+      case 'A':
+        printf("%d: 'A'\n", i);
+        break;
+
+      case 'e':
+      case 'E':
+        printf("%d: 'E'\n", i);
+        break;
+
+      case 'i':
+      case 'I':
+        printf("%d: 'I'\n", i);
+        break;
+
+      case 'o':
+      case 'O':
+        printf("%d: 'O'\n", i);
+        break;
+
+      case 'u':
+      case 'U':
+        printf("%d: 'U'\n", i);
+        break;
+
+      case 'y':
+      case 'Y':
+        if (i > 2) {
+          printf("%d: 'Y'\n", i);
+        }
+        break;
+      default:
+        printf("%d: %c is not a vowel\n", i, letter);
+    }
+  }
+
+  return 0;
 }
 
