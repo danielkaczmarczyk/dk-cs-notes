be35eedfbc645ba72ee0f3dfb3f19f8413a66c2e
daniel <danielkaczmarczyk2@gmail.com>
2021-01-26 17:49:10 -0800

    working through 12, checkpoint

--- c/hard_way/ex12.c
+++ c/hard_way/ex12.c
@@ -7,9 +7,19 @@ int main(int argc, char *argv[])
   char full_name[] = {
     'Z', 'e', 'd',
     ' ', 'A', '.', ' ',
-    'S', 'h','a', 'w', '\0'
-  }
+    'S', 'h', 'a', 'w',
+    '\0'
+  };
 
   printf("The size of an int: %ld\n", sizeof(int));
-  printf("The size of areas[] (int[]): %ld\n", sizeof(int));
+  printf("The size of areas[] (int[]): %ld\n", sizeof(areas));
+  printf("The number of ints in areas[]: %ld\n",
+    sizeof(areas) / sizeof(int)); // 20 bytes, 4 per int
+
+  printf("The size of char is: %ld\n", sizeof(char));
+  printf("The size of char name[]: %ld\n", sizeof(name));
+  printf("The number of chars: %ld\n",
+      sizeof(name) / sizeof(char));
+
+  printf("The size of full_name (char[]) is: %ld\n", sizeof(full_name));
 }
