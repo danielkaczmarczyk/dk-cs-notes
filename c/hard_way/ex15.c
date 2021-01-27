70a61b2755afc394b08f33061cc507d2ed543ac3
daniel <danielkaczmarczyk2@gmail.com>
2021-01-27 15:47:55 -0800

    slowly moving all print loops to fns

--- c/hard_way/ex15.c
+++ c/hard_way/ex15.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 
+void print_1(int ages[], char *names[], int count) {
+  for (int i = 0; i < count; i++) {
+    printf("%s has %d years alive\n", *(names + i), *(ages + i));
+  }
+  printf("--->\n");
+}
+
+
+void print_2(int ages[], char *names[], int count) {
+  int *cur_age = ages;
+  char **cur_name = names;
+
+  // printing second way using pointers
+  for (int i = 0; i < count; i++) {
+    // printf("cur_name pointer address: %p\n", (&cur_name + i));
+    // printf("cur_age  pointer address: %p\n", (&cur_age + i));
+    printf("%s is %d years old\n", *(cur_name + i), *(cur_age + i));
+  }
+
+  printf("--->\n");
+}
+
+
+void print_3(int ages[], char *names[], int count) {
+  int *cur_age = ages;
+  char **cur_name = names;
+  for (int i = 0; i < count; i++ ) {
+    printf("%s is %d years old\n", cur_name[i], cur_age[i]);
+  }
+
+  printf("--->\n");
+}
+
 int main(int argc, char *argv[]) {
   // char *argv[] is an array of pointers to chars (strings)
 
@@ -12,48 +45,26 @@ int main(int argc, char *argv[]) {
   int count = sizeof(ages) / sizeof(int);
   int i = 0;
 
-  // printing
-  // first way using indexing
-  for (i = 0; i < count; i++) {
-    printf("%s has %d years alive\n", *(names + i), *(ages + i));
-  }
+  print_1(ages, names, count);
+  print_2(ages, names, count);
+  print_3(ages, names, count);
 
-  printf("--->\n");
 
-  int *cur_age = ages;
-  char **cur_name = names;
-
-  // printing second way using pointers
-  for (i = 0; i < count; i++) {
-    printf("cur_name pointer address: %p\n", (&cur_name + i));
-    printf("cur_age  pointer address: %p\n", (&cur_age + i));
-    printf("%s is %d years old\n", *(cur_name + i), *(cur_age + i));
-  }
-
-  printf("--->\n");
-
-  // third way, pointers are 'just arrays'
-  for (i = 0; i < count; i++ ) {
-    printf("%s is %d years old\n", cur_name[i], cur_age[i]);
-  }
-
-  printf("--->\n");
-
-  // fourth way with pointers in a stupid complex way
-  for (
-      // make cur_name point at the beginning of names
-      // make cur_age point at the beginning of ages
-      cur_name = names, cur_age = ages;
-      // what the fuck is (cur_age - ages)
-      // alright, it's pointer artihmetic expression checking how far
-      // are we into the array
-      (cur_age - ages) < count;
-      // increment the pointers to move to the next block of memory
-      cur_name += 1, cur_age++
-  ) {
-    //printf("cur_age - ages: %ld\n", cur_age - ages);
-    printf("%s is %d years old\n", *cur_name, *cur_age);
-  }
+  // // fourth way with pointers in a stupid complex way
+  // for (
+  //     // make cur_name point at the beginning of names
+  //     // make cur_age point at the beginning of ages
+  //     cur_name = names, cur_age = ages;
+  //     // what the fuck is (cur_age - ages)
+  //     // alright, it's pointer artihmetic expression checking how far
+  //     // are we into the array
+  //     (cur_age - ages) < count;
+  //     // increment the pointers to move to the next block of memory
+  //     cur_name += 1, cur_age++
+  // ) {
+  //   //printf("cur_age - ages: %ld\n", cur_age - ages);
+  //   printf("%s is %d years old\n", *cur_name, *cur_age);
+  // }
 
 }
 
