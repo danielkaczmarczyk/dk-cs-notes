9bfc1d7af4296b5686b890cf88f161fdfe0943f6
daniel <danielkaczmarczyk2@gmail.com>
2021-01-27 10:31:33 -0800

    fooled around with ex13. moving on

--- c/hard_way/ex13.c
+++ c/hard_way/ex13.c
@@ -4,23 +4,27 @@ int main(int argc, char *argv[])
 {
   int i = 0;
 
+  char *states[] = {
+    "California",
+    "Oregon",
+    NULL,
+    "Maine"
+  };
+
+  argv[1] = states[3];
+
   for (i = 1; i < argc; i++)
   {
     printf("arg %d: %s\n", i, argv[i]);
   }
 
-  char *states[] = {
-    "California",
-    "Oregon",
-    "Texas",
-    "Maine"
-  };
+
+  states[2] = argv[0];
 
   int num_states = 4;
-
-  for (i = 0; i < num_states; i++) {
-    printf("State %d: %s\n", i, states[i]);
+  char *state;
+  for (i = 0; i < num_states && (state = states[i]); i++) {
+    printf("State %d: %s\n", i, state);
   }
-  
   return 0;
 }
