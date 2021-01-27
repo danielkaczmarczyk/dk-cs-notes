aca750c38265b29b346b0a2c48b7295a5c072cc2
daniel <danielkaczmarczyk2@gmail.com>
2021-01-26 16:27:38 -0800

    handling multiple args now

--- c/hard_way/ex10.c
+++ c/hard_way/ex10.c
@@ -3,47 +3,47 @@
 
 int main(int argc, char *argv[]) {
   
-  if (argc != 2) {
-    printf("Error! You need one argument.\n");
-    // abort the program
+  if (argc < 2) {
+    printf("Error! You need at least one argument.\n");
     return 1;
   }
 
-  int i = 0;
   char letter;
-  for (i = 0; (letter = argv[1][i]) != '\0'; i++) {
+  for (int j = 1; j < argc; j++) {
+    printf("\n--- %s:\n", argv[j]);
+    for (int i = 0; (letter = argv[j][i]) != '\0'; i++) {
+      if (isupper(letter)) {
+        letter = tolower(letter);
+      }
 
-    if (isupper(letter)) {
-      letter = tolower(letter);
-    }
+      switch (letter) {
+        case 'a':
+          printf("%d: 'A'\n", i);
+          break;
 
-    switch (letter) {
-      case 'a':
-        printf("%d: 'A'\n", i);
-        break;
+        case 'e':
+          printf("%d: 'E'\n", i);
+          break;
 
-      case 'e':
-        printf("%d: 'E'\n", i);
-        break;
+        case 'i':
+          printf("%d: 'I'\n", i);
+          break;
 
-      case 'i':
-        printf("%d: 'I'\n", i);
-        break;
+        case 'o':
+          printf("%d: 'O'\n", i);
+          break;
 
-      case 'o':
-        printf("%d: 'O'\n", i);
-        break;
+        case 'u':
+          printf("%d: 'U'\n", i);
+          break;
 
-      case 'u':
-        printf("%d: 'U'\n", i);
-        break;
+        case 'y':
+          printf("%d: 'Y'\n", i);
+          break;
 
-      case 'y':
-        printf("%d: 'Y'\n", i);
-        break;
-
-      default:
-        printf("%d: %c is not a vowel\n", i, letter);
+        default:
+          printf("%d: %c is not a vowel\n", i, letter);
+      }
     }
   }
 
