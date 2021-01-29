2c453dc2f87eba34c32bc74b1f8d4c3b2800d56c
daniel <danielkaczmarczyk2@gmail.com>
2021-01-29 13:57:02 -0800

    started on the database project

--- c/hard_way/ex17.c
+++ c/hard_way/ex17.c
@@ -14,7 +14,38 @@ struct Address {
   char email[MAX_DATA];
 };
 
+struct Database {
+  struct Address rows[MAX_ROWS];
+};
+
+struct Connection {
+  FILE *file;
+  struct Database *db;
+};
+
+void die(const char *message) {
+  // TODO what are those functions?
+  if (errno) {
+    perror(message);
+  } else {
+    printf("ERROR: %s\n", message);
+  }
+
+  exit(1);
+}
+
+void Address_print(struct Address *addr) {
+  printf("%d %s %s\n", addr->id, addr->name, addr->email);
+}
+
+void Database_load(struct Connection *conn) {
+  int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
+  if (rc != 1) {
+    die("Failed to load database.");
+  }
+}
+
 int main(int argc, char *argv[]) {
-  printf("i am the function\n");
+
 }
 
