22fb6d99854834f488d5bef69d34dbe0136b2f5f
daniel <danielkaczmarczyk2@gmail.com>
2021-01-29 14:34:16 -0800

    typed that boi in

--- c/hard_way/ex17.c
+++ c/hard_way/ex17.c
@@ -159,6 +159,59 @@ void Database_list(struct Connection *conn) {
 }
 
 int main(int argc, char *argv[]) {
-  
+  if (argc < 3) {
+    die("USAGE: ex17 <dbfile> <action> [action params]");
+  } 
+
+  char *filename = argv[1];
+  char action = argv[2][0];
+  struct Connection *conn = Database_open(filename, action);
+  int id = 0;
+
+  if (argc > 3) id = atoi(argv[3]);
+  if (id >= MAX_ROWS) die("There's not that many records.");
+
+  switch(action) {
+    case 'c':
+      Database_create(conn);
+      Database_write(conn);
+      break;
+
+    case 'g':
+      if (argc != 4) {
+        die("Need an id to get");
+      }
+      Database_get(conn, id);
+      break;
+
+    case 's':
+      if (argc != 6) {
+        die("Need id, name, email to set"); 
+      }
+
+      Database_set(conn, id, argv[4], argv[5]);
+      Database_write(conn);
+      break;
+
+    case 'd':
+      if (argc != 4) {
+        die("Need id to delete");
+      }
+
+      Database_delete(conn, id);
+      Database_write(conn);
+      break;
+
+    case 'l':
+      Database_list(conn);
+      break;
+
+    default:
+      die("Invalid action. Try c=create, g=get, s=set, d=delete, l=list");
+  }
+
+  Database_close(conn);
+
+  return 0;
 }
 
