f7e456e85bba1a8aa42b6e3131ac4b2ee8d42826
daniel <danielkaczmarczyk2@gmail.com>
2021-01-28 17:35:58 -0800

    started on ex17 - created ex file and amended make

---
+++ c/hard_way/ex17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
+struct Address {
+  int id;
+  int set;
+  char name[MAX_DATA];
+  char email[MAX_DATA];
+}
+
+int main(int argc, char *argv[]) {
+  printf("i am the function\n");
+}
+
