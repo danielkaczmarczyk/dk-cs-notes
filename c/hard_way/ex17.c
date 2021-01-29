b77cee5abdd9a6f57f727a383e8c44fee4dc68e5
daniel <danielkaczmarczyk2@gmail.com>
2021-01-29 14:05:04 -0800

    added Database_create

--- c/hard_way/ex17.c
+++ c/hard_way/ex17.c
@@ -99,6 +99,17 @@ void Database_write(struct Connection *conn) {
   }
 }
 
+void Database_create(struct Connection *conn) {
+  int i = 0;
+
+  for (i = 0; i < MAX_ROWS; i++) {
+    // make a prototype to initialize it
+    struct Address addr = { .id = i, .set = 0 };
+    // then just assign it
+    conn->db->rows[i] = addr;
+  }
+}
+
 int main(int argc, char *argv[]) {
 
 }
