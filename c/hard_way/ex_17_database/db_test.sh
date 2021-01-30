64400ec553aab2e7da7fa8237603dc17f7062c6e
daniel <danielkaczmarczyk2@gmail.com>
2021-01-29 17:17:34 -0800

    working more on the database test script

--- c/hard_way/ex_17_database/db_test.sh
+++ c/hard_way/ex_17_database/db_test.sh
@@ -1,3 +1,20 @@
 #!/bin/zsh
 
-./ex17
+TEST_DB_FILE="./db.dat"
+
+echo "Database test in progress... \n\n"
+
+# if the test db file exists, delete it
+if test -f "$TEST_DB_FILE"; then
+  rm $TEST_DB_FILE
+  echo '_>_> removed `./db.dat`'
+fi
+
+# populate the db
+# view data
+# alter data
+# view data
+# remove data
+# view data
+# cleanup
+
