84df7b9380f6aca30eb90a5e9e5a80727bfdfef0
daniel <danielkaczmarczyk2@gmail.com>
2021-01-31 13:55:30 -0800

    structs with pointers instead of defined arrays exercise

--- c/hard_way/ex_17_database/practice/practice.c
+++ c/hard_way/ex_17_database/practice/practice.c
@@ -5,14 +5,28 @@
 #include <string.h>
 
 #define VERBOSE 0
+#define MAX_DATA 512
 
 struct Address {
   int id;
   int set;
+  char name[MAX_DATA];
+  char email[MAX_DATA];
 };
 
-int main(int argc; char *argv[]) {
+void Address_print(struct Address *add) {
+  printf("id: %d\n", add->id);
+  printf("set: %d\n", add->set);
+  printf("name: %s\n", add->name);
+  printf("email: %s\n", add->email);
+
+}
+
+int main(int argc, char *argv[]) {
   struct Address my_address;
   my_address.id = 3;
   my_address.set = 1;
+  strcpy( my_address.name, "Daniel Kaczmarczyk");
+  strcpy( my_address.email, "daniel@dan.dk");
+  Address_print(&my_address);
 }
