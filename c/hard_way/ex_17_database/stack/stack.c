bffd085f0cbec2970231941e0a99c75fc152d3b6
daniel <danielkaczmarczyk2@gmail.com>
2021-02-01 11:16:36 -0800

    initial functionality works

--- c/hard_way/ex_17_database/stack/stack.c
+++ c/hard_way/ex_17_database/stack/stack.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct stack {
   int n_items;
@@ -7,13 +8,21 @@ struct stack {
 
 struct stack *new() {
   struct stack *s = malloc(sizeof(struct stack));
+  s->n_items = 0;
   return s;
 }
 
-void push(const char item) {
-
+void print(struct stack *s) {
+  printf("stack { items: %d } [ ", s->n_items);
+  for (int i = 0; i < s->n_items; i++ ) {
+    printf("%c, ", s->items[i]);
+  }
+  printf("]\n");
 }
+
 
 int main(int argc, char *argv[]) {
   struct stack *s = new();
+  print(s);
 }
+
