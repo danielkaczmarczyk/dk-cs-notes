aaa6f4df9d4d1eda520916f4ef98bba9bad910c0
daniel <danielkaczmarczyk2@gmail.com>
2021-02-02 11:39:26 -0800

    typedef for fake types; function pointers

--- c/hard_way/ex18.c
+++ c/hard_way/ex18.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+void die(const char *message) {
+  if (errno) {
+    perror(message);
+  } else {
+    printf("Error: %s\n", message);
+  }
+
+  exit(1);
+}
+
+// a typedef creates a fake type,
+// in this case for a function pointer
+typedef int (*compare_cb) (int a, int b);
 
 int main(int argc, char *argv[]) {
-  printf("Hello\n");
 }
+
