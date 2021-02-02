b86b5549f2df67c5cddaedf45636d4fa7276f285
daniel <danielkaczmarczyk2@gmail.com>
2021-02-02 11:51:17 -0800

    copied the code over; sorts wrong lol

--- c/hard_way/ex18.c
+++ c/hard_way/ex18.c
@@ -44,11 +44,59 @@ int *bubble_sort(int *numbers, int count, compare_cb cmp) {
   return target;
 }
 
+int sorted_order(int a, int b) {
+  return (a > b) - (a < b);
+}
 
+int reverse_order(int a, int b) {
+  return (a < b) - (a > b);
+}
 
+int strange_order(int a, int b) {
+  if (a == 0 || b == 0) {
+    return 0;
+  } else {
+    return a % b;
+  }
+}
 
+/**
+ * Used to teset that we are sorting things correctly by doing the sort
+ * and printing it out.
+ */
+void test_sorting(int *numbers, int count, compare_cb cmp) {
+  int i = 0;
+  int *sorted = bubble_sort(numbers, count, cmp);
 
+  if (!sorted)
+    die("Failed to sort as requested.");
+
+  for (i = 0; i < count; i++) {
+    printf("%d ", sorted[i]);
+  }
+  printf("\n");
+  free(sorted);
+}
 
 int main(int argc, char *argv[]) {
+  if (argc < 2) die("USAGE: ex18 4 3 1 5 6");
+
+  int count = argc -1;
+  int i = 0;
+  char **inputs = argv + 1;
+
+  int *numbers = malloc(count * sizeof(int));
+  if (!numbers) die("Memory error.");
+
+  for (i = 0; i < count; i++) {
+    numbers[i] = atoi(inputs[i]);
+  }
+
+  test_sorting(numbers, count, sorted_order);
+  test_sorting(numbers, count, reverse_order);
+  test_sorting(numbers, count, strange_order);
+
+  free(numbers);
+  return 0;
 }
 
