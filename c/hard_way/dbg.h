2af53b29fd1f2b93ce40bcc0757d63176aeaf99c
daniel <danielkaczmarczyk2@gmail.com>
2021-02-02 15:30:40 -0800

    logging filenames too

--- c/hard_way/dbg.h
+++ c/hard_way/dbg.h
@@ -15,15 +15,15 @@
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
 #define log_err(M, ...) fprintf(stderr, \
-    "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
+    "[ERROR] (%s on %s:%d: errno: %s) " M "\n", __func__, __FILE__, __LINE__,\
     clean_errno(), ##__VA_ARGS__)
 
 #define log_warn(M, ...) fprintf(stderr, \
-    "[WARN] (%s:%d: errno: %s) " M "\n",\
-    __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+    "[WARN] (%s on %s:%d: errno: %s) " M "\n",\
+    __func__, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
-    __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s on %s:%d) " M "\n",\
+    __func__, __FILE__, __LINE__, ##__VA_ARGS__)
 
 #define check(A, M, ...) if(!(A)) {\
   log_err(M, ##__VA_ARGS__); errno=0; goto error; }
