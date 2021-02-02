1aef9fe07d5ab6c0a48ac23dc9e94e38252c1708
daniel <danielkaczmarczyk2@gmail.com>
2021-02-02 15:01:03 -0800

    init test debug and log err

--- c/hard_way/ex20.c
+++ c/hard_way/ex20.c
@@ -6,8 +6,19 @@ void test_debug()
 {
   // notice you don't need the \n
   debug("I have brown hair.");
+
+  // passing in arguments like printf
+  debug("I am %d years old.", 37);
 }
+
+void test_log_err()
+{
+  log_err("I believe everything is broken.");
+  log_err("There are %d problems in %s", 0, "space");
+}
+
 
 int main(int argc, char *argv[]) {
   test_debug();
+  test_log_err();
 }
