4ebdb113565122686cf41a43113c7e039d0e29a1
daniel <danielkaczmarczyk2@gmail.com>
2021-02-02 14:41:20 -0800

    NULL-d the sorting callback to cause segfault

--- c/hard_way/ex18.c
+++ c/hard_way/ex18.c
@@ -130,7 +130,7 @@ int main(int argc, char *argv[])
         numbers[i] = atoi(inputs[i]);
     }
 
-    test_sorting(numbers, count, sorted_order);
+    test_sorting(numbers, count, NULL);
     test_sorting(numbers, count, reverse_order);
     test_sorting(numbers, count, strange_order);
 
