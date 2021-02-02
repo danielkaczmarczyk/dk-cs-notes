d3f71948639c6d6e2ef0002115c7bcfec37ebbd9
daniel <danielkaczmarczyk2@gmail.com>
2021-02-02 11:43:28 -0800

    written the sort

--- c/hard_way/ex18.c
+++ c/hard_way/ex18.c
@@ -17,6 +17,38 @@ void die(const char *message) {
 // in this case for a function pointer
 typedef int (*compare_cb) (int a, int b);
 
+/**
+ * A classic bubble sort function that uses the compare_cb to do sorting.
+ */
+int *bubble_sort(int *numbers, int count, compare_cb cmp) {
+  int temp = 0;
+  int i = 0;
+  int j = 0;
+  int *target = malloc(count * sizeof(int));
+
+  if (!target)
+    die("Memory error.");
+
+  memcpy(target, numbers, count * sizeof(int));
+
+  for (i = 0; i < count; i++) {
+    for (j = 0; j < count - 1; j++) {
+      if (cmp(target[j], target[j + 1] > 0)) {
+        temp = target[j + 1];
+        target[j + 1] = target[j];
+        target[j] = temp;
+      }
+    }
+  }
+
+  return target;
+}
+
+
+
+
+
+
 int main(int argc, char *argv[]) {
 }
 
