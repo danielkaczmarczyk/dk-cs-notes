09f514acb4caca34fa9ded89a1c463da50873fcf
daniel <danielkaczmarczyk2@gmail.com>
2021-02-03 15:58:36 -0800

    read_int and read_string input

--- c/hard_way/ex25.c
+++ c/hard_way/ex25.c
@@ -8,4 +8,35 @@
 int read_string(char **out_string, int max_buffer)
 {
   *out_string = calloc(1, max_buffer + 1);
+  check_mem(*out_string);
+
+  char *result = fgets(*out_string, max_buffer, stdin);
+  check(result != NULL, "Input error.");
+
+  return 0;
+
+error:
+  if (*out_string) free(*out_string);
+  *out_string = NULL;
+  return -1;
 }
+
+int read_int(long *out_int)
+{
+  char *input = NULL;
+  char *end = NULL;
+  int rc = read_string(&input, MAX_DATA);
+  check(rc == 0, "Failed to read number.");
+
+  *out_int = strtol(input, &end, 10);
+  check((*end == '\0' || *end == '\n') && *input != '\0', "Invalid number: %s", input);
+
+  free(input);
+  return 0;
+
+error:
+  if (input) free(input);
+  return -1;
+}
+
+
