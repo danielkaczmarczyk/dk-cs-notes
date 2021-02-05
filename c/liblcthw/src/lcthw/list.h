fd6e81359256d378137179d8d8c33d186011b8e6
daniel <danielkaczmarczyk2@gmail.com>
2021-02-05 14:42:33 -0800

    init doubly linked list

---
+++ c/liblcthw/src/lcthw/list.h
@@ -0,0 +1,19 @@
+#ifndef lcthw_List_h
+#define lcthw_List_h
+
+#include <stdlib.h>
+
+struct ListNode;
+
+typedef struct ListNode {
+  struct ListNode *next;
+  struct ListNode *prev;
+  void *value;
+} ListNode;
+
+typedef struct List {
+  int count;
+  ListNode *first;
+  ListNode *last;
+} List;
+
