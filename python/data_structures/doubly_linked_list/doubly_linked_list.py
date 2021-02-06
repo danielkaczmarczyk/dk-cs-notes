6d6ef91fb18b6d8d4b601f74afbd1db10e2a7cbf
daniel <danielkaczmarczyk2@gmail.com>
2021-02-05 17:17:29 -0800

    tested print

--- python/data_structures/doubly_linked_list/doubly_linked_list.py
+++ python/data_structures/doubly_linked_list/doubly_linked_list.py
@@ -32,12 +32,17 @@ class DoublyLinkedList():
         pass
 
     def print(self):
-        node = self.last
+        result = ''
+        node = self.first
         while node != None:
-            print(node.value)
+            result += f"{node.value} -> "
             node = node.next
+        result += "None"
+        return result
 
-
+    def batch_populate(self, items):
+        for item in items:
+            self.push(item)
 
 
 class ListNode():
@@ -47,8 +52,7 @@ class ListNode():
         self.value = value
 
 
-# tests - to be moved out to a diff file (with an actual testing framework)
-
+# tests -----------------------------------------------------------------------
 import unittest
 
 class TestList(unittest.TestCase):
@@ -56,6 +60,40 @@ class TestList(unittest.TestCase):
     def test_init(self):
         dllist = DoublyLinkedList()
         self.assertIsInstance(dllist, DoublyLinkedList)
+        self.assertIsNone(dllist.first)
+        self.assertIsNone(dllist.last)
+        self.assertEqual(dllist.count, 0)
+
+    def test_push(self):
+        dllist = DoublyLinkedList()
+
+        dllist.push('a')
+        self.assertIsNotNone(dllist.first)
+        self.assertEqual(dllist.first.value, 'a')
+
+        self.assertIsNotNone(dllist.last)
+        self.assertEqual(dllist.last.value, 'a')
+
+        self.assertEqual(dllist.count, 1)
+
+        dllist.push('b')
+
+        self.assertEqual(dllist.first.value, 'a')
+        self.assertEqual(dllist.last.value, 'b')
+        self.assertEqual(dllist.count, 2)
+        
+        dllist.push('c')
+        self.assertEqual(dllist.first.value, 'a')
+        self.assertEqual(dllist.last.value, 'c')
+        self.assertEqual(dllist.count, 3)
+
+    def test_print(self):
+        dllist = DoublyLinkedList()
+        dllist.batch_populate(['a', 'b', 'c'])
+        printout = dllist.print()
+        self.assertEqual(printout, "a -> b -> c -> None")
+
+
 
 if __name__ == '__main__':
     unittest.main()
