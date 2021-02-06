745ba593add3875ef18cd6075fa0420bcac6ae7b
daniel <danielkaczmarczyk2@gmail.com>
2021-02-05 16:42:42 -0800

    init on doubly linked lists

--- python/data_structures/doubly_linked_list/doubly_linked_list.py
+++ python/data_structures/doubly_linked_list/doubly_linked_list.py
@@ -1,2 +1,13 @@
-class DoublyLinkedList():
-    pass
+class List():
+    def __init__():
+        self.count = 0
+        self.first = None
+        self.last = None
+
+
+class ListNode():
+    def __init__():
+        self.next = None
+        self.prev = None
+        self.value = None
+
