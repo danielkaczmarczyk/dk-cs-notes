29f74f65c29ebb264abf660106cbb73bc27cd50c
daniel <danielkaczmarczyk2@gmail.com>
2021-02-10 11:01:42 -0800

    init unordered_list

---
+++ practical_algorithms_and_data_structures/05_lists/unordered_list.py
@@ -0,0 +1,33 @@
+class UnorderedList():
+    def __init__(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def remove(self, item):
+        pass
+
+    def search(self, item):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
+
+    def append(self, item):
+        pass
+
+    def index(self, item):
+        pass
+
+    def insert(self, pos, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def pop(self, pos):
+        pass
