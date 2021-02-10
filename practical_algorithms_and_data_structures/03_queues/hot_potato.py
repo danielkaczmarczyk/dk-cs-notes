a8b5f541484869fca9ed38983a62502a376dc89d
daniel <danielkaczmarczyk2@gmail.com>
2021-02-09 21:34:49 -0800

    added the hot potao game sim, come play

---
+++ practical_algorithms_and_data_structures/03_queues/hot_potato.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+def hot_potato(names, num):
+    queue = deque()
+
+    for name in names:
+        queue.appendleft(name)
+
+    while len(queue) > 1:
+        for _ in range(num):
+            queue.appendleft(queue.pop())
+
+        queue.pop()
+
+    return queue.pop()
+
+if __name__ == '__main__':
+    names = ['Bill', 'David', 'Susan', 'Jane', 'Kent', 'Brad']
+    print(hot_potato(names, 9))
