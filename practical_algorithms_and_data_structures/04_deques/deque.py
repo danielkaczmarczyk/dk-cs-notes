126f0e97c725a5b9edf0a0bd541864183148b593
daniel <danielkaczmarczyk2@gmail.com>
2021-02-09 21:36:50 -0800

    deque init

---
+++ practical_algorithms_and_data_structures/04_deques/deque.py
@@ -0,0 +1,25 @@
+class Deque():
+    def __init__(self):
+        pass
+
+    def add_front(self, item):
+        pass
+
+    def add_read(self, item):
+        pass
+
+    def remove_front(self):
+        pass
+
+    def remove_rear(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
