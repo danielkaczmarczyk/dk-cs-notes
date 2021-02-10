c5ffc5a1aa13ddc041f97dedf92931b6326dbe2f
daniel <danielkaczmarczyk2@gmail.com>
2021-02-09 18:33:50 -0800

    sums timed

--- practical_algorithms_and_data_structures/01_analysis/sum_to_n.py
+++ practical_algorithms_and_data_structures/01_analysis/sum_to_n.py
@@ -1,10 +1,25 @@
+import time
+
 def sum_to_n(n):
+    start = time.time()
+
     total = 0
     for i in range(n + 1):
         total += i
-    return total
+
+    end = time.time()
+    return total, end - start
+
+def smarter_sum(n):
+    start = time.time()
+    result = (n * (n + 1)) / 2
+    return  result, time.time() - start
 
 if __name__ == "__main__":
-    n = 3
-
-    print(sum_to_n(n))
+    n = 1_000_000
+    for _ in range(15):
+        result, t = sum_to_n(n)
+        result_2, t_2 = smarter_sum(n)
+        print(f"sum_to_n({n}) = {result}, in: {t} seconds")
+        print(f"smarter_sum({n}) = {result_2}, in: {t_2} seconds")
+        print()
