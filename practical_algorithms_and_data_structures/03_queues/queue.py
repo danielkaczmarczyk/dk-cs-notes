6755e3fe12632a2d81b0fa0d4e7037e5430b7a56
daniel <danielkaczmarczyk2@gmail.com>
2021-02-09 21:25:14 -0800

    queue init

---
+++ practical_algorithms_and_data_structures/03_queues/queue.py
@@ -0,0 +1,15 @@
+class Queue():
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
