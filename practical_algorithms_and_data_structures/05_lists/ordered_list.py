107ad1de77dbc67ab1326dafb3024f4c7f8da95f
daniel <danielkaczmarczyk2@gmail.com>
2021-02-10 17:28:32 -0800

    init work on the ordered list

---
+++ practical_algorithms_and_data_structures/05_lists/ordered_list.py
@@ -0,0 +1,24 @@
+from unordered_list import Node, UnorderedList
+
+
+class OrderedList(UnorderedList):
+
+    def search(self):
+        pass
+
+    def add(self):
+        pass
+
+
+if __name__ == '__main__':
+    import unittest
+
+    
+    class OrderedListTests(unittest.TestCase):
+
+        def test_search(self):
+            pass
+
+        def test_add(self):
+            pass
+
