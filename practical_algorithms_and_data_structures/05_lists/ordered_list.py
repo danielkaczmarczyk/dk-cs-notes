21b6c1ce0429a89110e4f4cf9ca1621109f78d4c
daniel <danielkaczmarczyk2@gmail.com>
2021-02-10 18:01:31 -0800

    ok seems to be adding them correctly

--- practical_algorithms_and_data_structures/05_lists/ordered_list.py
+++ practical_algorithms_and_data_structures/05_lists/ordered_list.py
@@ -4,10 +4,38 @@ from unordered_list import Node, UnorderedList
 class OrderedList(UnorderedList):
 
     def search(self):
-        pass
 
-    def add(self):
-        pass
+        current = self.head
+
+        while current is not None:
+            print(current.value)
+            current = current.next
+
+    def add(self, item):
+        node = Node(item)
+        if self.head is None:
+            self.head = node
+        else:
+            previous = None
+            current = self.head
+
+            # case where the value is the smallest in the list.
+            if current.value > node.value:
+                node.next = self.head
+                self.head = node
+                return
+
+            # while we're not at the end and the values are still rising
+            while current is not None and current.value < node.value:
+                previous, current = current, current.next
+
+            # how do we handle bigger/smaller? in that case it only works if
+            # the node we're inserting is smaller than the last node.
+            node.next = current
+
+            # ensure that we're not at the beginning
+            if previous is not None:
+                previous.next = node
 
 
 if __name__ == '__main__':
@@ -18,15 +46,19 @@ if __name__ == '__main__':
 
         def list_setup(self):
             ol = OrderedList()
-
-            for n in [26, 31, 17, 54, 93, 77]:
+            for n in [26, 31, 17, 54, 93, 77, 101]:
                 ol.add(n)
-
             return ol
 
         def test_search(self):
             pass
 
         def test_add(self):
-            pass
+            ol = self.list_setup()
+
+            self.assertEqual(ol.size(), 7)
+            ol.search()
+
+
+    unittest.main()
 
