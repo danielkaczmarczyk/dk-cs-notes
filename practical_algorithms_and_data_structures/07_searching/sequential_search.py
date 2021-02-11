788c932db2e8778a0c4ecf9fb15ef7d326ccb535
daniel <danielkaczmarczyk2@gmail.com>
2021-02-11 12:17:59 -0800

    ordered implementation saving on steps

--- practical_algorithms_and_data_structures/07_searching/sequential_search.py
+++ practical_algorithms_and_data_structures/07_searching/sequential_search.py
@@ -4,9 +4,28 @@ def sequential_search(array, item):
             return True
     return False
 
+def ordered_sequential_search(alist, item):
+    position = 0
+
+    while position < len(alist):
+        if alist[position] == item:
+            return True
+
+        if alist[position] > item:
+            return False
+
+        position = position + 1
+
+    return False
+
+
 if __name__ == '__main__':
     array = [1, 15, 253, 4306, 543, 603, 43, 0, 436]
     item = 543
     print(sequential_search(array, item))
     item = 5433
     print(sequential_search(array, item))
+
+    testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+    print(ordered_sequential_search(testlist, 3))
+    print(ordered_sequential_search(testlist, 13))
