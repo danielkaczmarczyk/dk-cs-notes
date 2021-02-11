7ffe8cb25a02f887fcc7e5d9099049d9fe497b85
daniel <danielkaczmarczyk2@gmail.com>
2021-02-11 11:58:34 -0800

    attempting the lattice challenge with a dynamic approach

--- practical_algorithms_and_data_structures/06_recursion/dynamic_programming.py
+++ practical_algorithms_and_data_structures/06_recursion/dynamic_programming.py
@@ -4,7 +4,7 @@ def recursive_fib(n):
         return 1
     return recursive_fib(n - 1) + recursive_fib(n - 2)
 
-def dynamic_fib(n):
+def dyn_fib(n):
     previous, current = 0, 1
     while previous < n:
         previous, current = current, previous + current
@@ -14,6 +14,9 @@ def rec_num_paths(h, w):
     if h == 0 or w == 0:
         return 1
     return rec_num_paths(h - 1, w) + rec_num_paths(h, w - 1)
+
+def dyn_num_paths(h, w):
+    pass
 
 
 if __name__ == '__main__':
