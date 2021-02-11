09de71d094bd5f110079540fbda10e41f0695204
daniel <danielkaczmarczyk2@gmail.com>
2021-02-10 17:29:59 -0800

    list_setup complete

--- practical_algorithms_and_data_structures/05_lists/ordered_list.py
+++ practical_algorithms_and_data_structures/05_lists/ordered_list.py
@@ -16,6 +16,14 @@ if __name__ == '__main__':
     
     class OrderedListTests(unittest.TestCase):
 
+        def list_setup(self):
+            ol = OrderedList()
+
+            for n in [26, 31, 17, 54, 93, 77]:
+                ol.add(n)
+
+            return ol
+
         def test_search(self):
             pass
 
