82c9173f62e7e001f16db3155f517d17a868f485
daniel <danielkaczmarczyk2@gmail.com>
2021-02-11 16:29:13 -0800

    played with a simple folding algorithm

--- practical_algorithms_and_data_structures/07_searching/folding.py
+++ practical_algorithms_and_data_structures/07_searching/folding.py
@@ -1,16 +1,20 @@
 """
     Folding method for constructing hash functions.
 """
-def fold(number):
+def fold(number, table_size):
     # make it a string 
     number = str(number)
+    container = []
 
     index = 0 
     step = 2
 
     while index < len(number):
         print(number[index])
+        container.append(int(number[index] + number[index + 1]))
         index += step
+
+    return sum(container) % table_size
 
 if __name__ == '__main__':
     import unittest
@@ -19,8 +23,9 @@ if __name__ == '__main__':
 
         def test_fold_1(self):
             number = 4365554601
-            result = fold(number)
-            expected = 10
+            table_size = 11
+            result = fold(number, table_size)
+            expected = 1
             message = f"folding {number} did not give {expected}, got {result} instead"
 
             self.assertEqual(result, expected, message)
