a9564612382b6529a0ab2b1a213afe2c26fb99d9
daniel <danielkaczmarczyk2@gmail.com>
2021-02-11 18:56:01 -0800

    outlined the rules

--- practical_algorithms_and_data_structures/08_trees/parse_tree_self.py
+++ practical_algorithms_and_data_structures/08_trees/parse_tree_self.py
@@ -1,4 +1,18 @@
 def build_tree(expression):
+    OPERATORS = ['*', '-', '+', '/']
+    LEFT_PAREN = '('
+    RIGHT_PAREN = ')'
+
+
+    for token in expression:
+        if token == LEFT_PAREN:
+            print('Add a new node as the left child of the current node, and descend to the left child')
+        elif token == RIGHT_PAREN:
+            print('Go to the parent of the current node')
+        elif isinstance(token, int):
+            print('Set the root value of the current node to the number and return to parent')
+        elif token in OPERATORS:
+            print('Set the root value of the current node to the operator represented by th ecurrent token. Add a new node as the right child of the current node and descend to the right child.')
     pass
 
 
