770bbd21f3ca121ea09060499ac33059022c94fc
daniel <danielkaczmarczyk2@gmail.com>
2021-02-11 16:31:02 -0800

    oops

--- practical_algorithms_and_data_structures/07_searching/mid_square_method.py
+++ practical_algorithms_and_data_structures/07_searching/mid_square_method.py
@@ -1,15 +1,27 @@
 """
-    Mid square folding method for constructing hash functions.
+    Folding method for constructing hash functions.
 """
 def fold(number, table_size):
-    pass
+    # make it a string 
+    number = str(number)
+    container = []
+
+    index = 0 
+    step = 2
+
+    while index < len(number):
+        print(number[index])
+        container.append(int(number[index] + number[index + 1]))
+        index += step
+
+    return sum(container) % table_size
 
 if __name__ == '__main__':
     import unittest
 
     class TestFold(unittest.TestCase):
 
-        def test_fold(self):
+        def test_fold_1(self):
             number = 4365554601
             table_size = 11
             result = fold(number, table_size)
