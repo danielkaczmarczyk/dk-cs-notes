2bc5b3d5e8b1ec54ecdb256d616243d299a29a54
daniel <danielkaczmarczyk2@gmail.com>
2021-02-11 19:16:00 -0800

    running into some kinda wonky error

--- practical_algorithms_and_data_structures/08_trees/parse_tree_self.py
+++ practical_algorithms_and_data_structures/08_trees/parse_tree_self.py
@@ -8,17 +8,33 @@ def build_tree(expression):
     current_node = tree
 
     for token in expression:
+        print(f"{stack=}")
         if token == LEFT_PAREN:
-            pass
+            new_node = {}
+            current_node['left'] = new_node
+            stack.append(new_node)
+            current_node = stack[-1]
         elif token == RIGHT_PAREN:
-            pass
-        elif isinstance(token, int):
-            pass
+            current_node = stack.pop()
         elif token in OPERATORS:
-            pass
+            current_node['val'] = token
+            new_node = {}
+            current_node['right'] = new_node
+            stack.append(new_node)
+            current_node = stack[-1]
+        elif isinstance(int(token), int):
+            current_node['val'] = token
+            current_node = stack.pop()
+        else:
+            print(f"THIS SHOULD NOT HAPPEN. {token=}")
+
+    print(f"{stack=}")
+    print(f"{tree=}")
+    return tree
 
 
 if __name__ == "__main__":
-    expression = ['(', '3', '+', '(', '4', '*', '5' ,')',')']
+    #expression = ['(', '3', '+', '(', '4', '*', '5' ,')',')']
+    expression = ['(', '2', '+', '2', ')']
     tree = build_tree(expression)
-    expected = {'val': '+', 'left': { 'val': 3 }, 'right': { 'val': 'x', 'left': { 'val': 4 }, 'right': { 'val': 5 }}}
+    #expected = {'val': '+', 'left': { 'val': 3 }, 'right': { 'val': 'x', 'left': { 'val': 4 }, 'right': { 'val': 5 }}}
