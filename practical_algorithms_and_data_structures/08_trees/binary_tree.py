31f10170e627c6ea218dc4e8395dabad4bd6bfcf
daniel <danielkaczmarczyk2@gmail.com>
2021-02-11 18:14:18 -0800

    inserting left and right

--- practical_algorithms_and_data_structures/08_trees/binary_tree.py
+++ practical_algorithms_and_data_structures/08_trees/binary_tree.py
@@ -4,5 +4,19 @@ class Node(object):
         self.left = None
         self.right = None
 
+    def insert_left(self, child):
+        if self.left is None:
+            self.left = child
+        else:
+            child.left = self.left
+            self.left = child
+
+    def insert_right(self, child):
+        if self.right is None:
+            self.right = child
+        else:
+            child.right = self.right
+            self.right = child
+
 if __name__ == '__main__':
     pass
