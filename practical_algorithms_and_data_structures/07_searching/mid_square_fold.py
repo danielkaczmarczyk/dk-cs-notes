9a019a93535aafd08f97d1110a3cefd024825ed4
daniel <danielkaczmarczyk2@gmail.com>
2021-02-11 16:36:22 -0800

    init mid square, this time for real

---
+++ practical_algorithms_and_data_structures/07_searching/mid_square_fold.py
@@ -0,0 +1,15 @@
+"""
+    Mid square folding method for constructing hash functions.
+"""
+def mid_square_fold(number, table_size):
+    number = str(number)
+
+if __name__ == '__main__':
+    import unittest
+
+    class TestFold(unittest.TestCase):
+
+        def test_fold(self):
+
+
+    unittest.main()
