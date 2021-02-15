ba6ba392221a725ddc9d7cb3f3b0777db398411c
daniel <danielkaczmarczyk2@gmail.com>
2021-02-14 21:04:36 -0800

    init binary_search_tree

--- practical_algorithms_and_data_structures/08_trees/binary_heap.py
+++ practical_algorithms_and_data_structures/08_trees/binary_heap.py
@@ -5,4 +5,45 @@ class BinaryHeap(object):
     def __len__(self):
         return len(self.items) - 1
 
+    def percolate_up(self):
+        i = len(self)
+        while i // 2 > 0:
+            if self.items[i] < self.items[i // 2]:
+                self.items[i // 2], self.items[i] = self.items[i], self.items[i //2]
+            i = i // 2
+
+    def insert(self, k):
+        self.items.append(k)
+        self.percolate_up()
+
+    def percolate_down(self, i):
+        while i * 2 <= len(self):
+            mc = self.min_child(i)
+            if self.items[i] > self.items[mc]:
+                self.items[i], self.items[mc] = self.items[mc], self.items[i]
+            i = mc
+
+    def min_child(self, i):
+        if i * 2 + 1 > len(self):
+            return i * 2
+
+        if self.items[i * 2] < self.items[i * 2 + 1]:
+            return i * 2
+
+        return i * 2 + 1
+
+    def delete_min(self):
+        return_value = self.items[1]
+        self.items[1] = self.items[len(self)]
+        self.items.pop()
+        self.percolate_down(1)
+        return return_value
+
+    def build_heap(self, alist):
+        i = len(alist) // 2
+        self.items = [0] + alist
+        while i > 0:
+            self.percolate_down(i)
+            i = i - 1
+
 
---
+++ practical_algorithms_and_data_structures/08_trees/binary_search_tree.py
@@ -0,0 +1,9 @@
+class TreeNode(object):
+
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        self.key = key
+        self.val = val
+        self.left = left
+        self.right = right
+        self.parent = parent
+
