34e751f612e24c73b93d778956662e94e32b4f93
daniel <danielkaczmarczyk2@gmail.com>
2021-02-14 21:24:04 -0800

    drafted the whole tree

--- practical_algorithms_and_data_structures/08_trees/binary_search_tree.py
+++ practical_algorithms_and_data_structures/08_trees/binary_search_tree.py
@@ -25,4 +25,68 @@ class TreeNode(object):
     def has_one_child(self):
         return self.has_any_children() and not self.has_both_children()
 
+    def replace_node_data(self, key, val, left, right):
+        self.key = key
+        self.val = val
+        self.left = left
+        self.right = right
+        if self.left:
+            self.left.parent = self
+        if self.right:
+            self.right.parent = self
+
+    def __iter__(self):
+        if self is None:
+            return
+
+        if self.left:
+            # `in` calls `__iter__` so is recursive
+            for elem in self.left:
+                yield elem
+
+        yield self.key
+
+        if self.right:
+           # recurse again
+           for elem in self.right:
+               yield elem
+
+    def find_successor(self):
+        if self.right:
+            return self.right.find_min()
+
+        if self.parent is None:
+            return None
+
+        if self.is_left_child():
+            return self.parent
+
+        self.parent.right = None
+        successor = self.parent.find_successor()
+        self.parent.right = self
+        return successor
+
+    def find_min(self):
+        current = self
+        while current.left:
+            current = current.lef
+        return current
+
+    def splice_out(self):
+        if self.is_leaf():
+            if self.is_left_child():
+                self.parent.left = None
+            else:
+                self.parent.right = None
+
+        else:
+            promoted_node = self.left or self.right
+
+            if self.is_left_child():
+                self.parent.left = promoted_node
+            else:
+                self.parent.right = promoted_node
+
+            promoted_node.parent = self.parent
+
 
