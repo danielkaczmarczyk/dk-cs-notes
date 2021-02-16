98d04f4100c3c30901ff0bbaa19c74d1b825295b
daniel <danielkaczmarczyk2@gmail.com>
2021-02-15 17:26:46 -0800

    added graph class definition

--- practical_algorithms_and_data_structures/09_graphs/graph.py
+++ practical_algorithms_and_data_structures/09_graphs/graph.py
@@ -14,3 +14,33 @@ class Vertex(object):
 
     def get_weight(self, neighbor):
         return self.neighbors[neighbor]
+
+def Graph(object):
+    def __init__(self):
+        self.verticies = {}
+
+    def add_vertex(self, vertex):
+        self.verticies[vertex.key] = vertex
+
+    def get_vertex(self, key):
+        try:
+            return self.verticies[key]
+        except KeyError:
+            return None
+
+    def __contains__(self, key):
+        return key in self.verticies
+
+    def add_edge(self, from_key, to_key, weight=0):
+        if from_key not in self.verticies:
+            self.add_vertex(Vertex(from_key))
+        if to_key not in self.verticies:
+            self.add_vertex(Vertex(to_key))
+        self.verticies[from_key].add_neighbor(self.verticies[to_key])
+
+    def get_verticies(self):
+        return self.verticies.keys()
+
+    def __iter__(self):
+        return iter(self.verticies.values())
+
