a244eae9b0cec37ce083e284ff71b045b70d13bd
daniel <danielkaczmarczyk2@gmail.com>
2021-02-15 19:04:39 -0800

    added ll and node classes

---
+++ educative/data_structures_and_algorithms_in_python/linked_list.py
@@ -0,0 +1,3 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
---
+++ educative/data_structures_and_algorithms_in_python/node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
