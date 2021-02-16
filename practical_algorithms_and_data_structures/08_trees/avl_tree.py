b54a68ffc6cd56b16e6d935a9f431c559fb6ee01
daniel <danielkaczmarczyk2@gmail.com>
2021-02-15 17:10:51 -0800

    init avl trees

---
+++ practical_algorithms_and_data_structures/08_trees/avl_tree.py
@@ -0,0 +1,7 @@
+from binary_search_tree import BinarySearchTree, TreeNode
+
+
+class AVLTreeNode(TreeNode):
+    def __init__(self, *args, **kwargs):
+        super(AVLTreeNode, self).__init__(*args, **kwargs)
+        self.balance_factor = 0
