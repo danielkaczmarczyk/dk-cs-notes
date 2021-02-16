17ec99f5883f1b491c16d53801f7b85ac25ea39b
daniel <danielkaczmarczyk2@gmail.com>
2021-02-16 14:27:34 -0800

    rotates but not far enough

--- educative/data_structures_and_algorithms_in_python/lists/right_rotate.py
+++ educative/data_structures_and_algorithms_in_python/lists/right_rotate.py
@@ -1,5 +1,17 @@
 def right_rotate(a_list, n):
-    pass
+    result = [None for x in a_list]
+
+    for index in range(n):
+        result[index + n - 1] = a_list[index]
+
+    for index in range(n, len(a_list)):
+        print(f"{index=}")
+        print(f"{a_list[index]=}")
+        location = index - n
+        print(f"{location=}")
+        result[location] = a_list[index]
+
+    return result
 
 if __name__ == '__main__':
     a_list = [10, 20, 30, 40, 50]
