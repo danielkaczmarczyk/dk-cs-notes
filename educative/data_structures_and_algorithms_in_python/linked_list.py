6cbf364fe105a6db0e1097823f04d8f226533e6a
daniel <danielkaczmarczyk2@gmail.com>
2021-02-15 19:24:16 -0800

    init tests on linked list

--- educative/data_structures_and_algorithms_in_python/linked_list.py
+++ educative/data_structures_and_algorithms_in_python/linked_list.py
@@ -1,3 +1,54 @@
+from node import Node
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def append(self, data):
+        node = Node(data)
+
+        if self.head is None:
+            self.head = node
+            return
+
+        current_node = self.head
+        while current_node.next is not None:
+            current_node = current_node.next
+
+        current_node.next = node
+
+    def __len__(self):
+        length = 0
+
+        current_node = self.head
+        while current_node is not None:
+            length += 1
+            current_node = current_node.next
+
+        return length
+
+
+if __name__ == '__main__':
+    import unittest
+
+    class TestLinkedList(unittest.TestCase):
+
+        def test_append(self):
+            ll = LinkedList()
+            ll.append("A")
+            self.assertEqual(ll.head.data, "A")
+
+            ll.append("B")
+
+        def test__len__(self):
+            ll = LinkedList()
+            self.assertEqual(len(ll), 0)
+
+            for letter in ["A", "B", "C"]:
+                ll.append(letter)
+
+            self.assertEqual(len(ll), 3)
+
+    unittest.main()
+
+
