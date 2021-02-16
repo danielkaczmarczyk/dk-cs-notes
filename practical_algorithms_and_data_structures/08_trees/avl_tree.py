bb61b6abe799c70cf74acdad4da83dbe8af957ba
daniel <danielkaczmarczyk2@gmail.com>
2021-02-15 17:12:47 -0800

    added _put for avlt

--- practical_algorithms_and_data_structures/08_trees/avl_tree.py
+++ practical_algorithms_and_data_structures/08_trees/avl_tree.py
@@ -5,3 +5,22 @@ class AVLTreeNode(TreeNode):
     def __init__(self, *args, **kwargs):
         super(AVLTreeNode, self).__init__(*args, **kwargs)
         self.balance_factor = 0
+
+
+class AVLTree(BinarySearchTree):
+
+    TreeNodeClass = AVLTreeNode
+
+    def _put(self, key, val, node):
+        if key < node.key:
+            if node.left:
+                self._put(key, val, node.left)
+            else:
+                node.left = self.TreeNodeClass(key, val, parent=node)
+                self.update_balance(node.left)
+        else:
+            if node.right:
+                self._put(key, val, node.right)
+            else:
+                node.right = self.TreeNodeClass(key, val, parent=node)
+                self.update_balance(node.right)
