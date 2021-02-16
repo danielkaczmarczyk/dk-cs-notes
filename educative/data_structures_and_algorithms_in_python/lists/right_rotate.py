f855f8374447856d4cf43f2194e182623632954b
daniel <danielkaczmarczyk2@gmail.com>
2021-02-16 14:42:18 -0800

    right rotate is buggy

--- educative/data_structures_and_algorithms_in_python/lists/right_rotate.py
+++ educative/data_structures_and_algorithms_in_python/lists/right_rotate.py
@@ -1,24 +1,58 @@
 def right_rotate(a_list, n):
     result = [None for x in a_list]
 
-    for index in range(n):
-        result[index + n - 1] = a_list[index]
+    for index in range(n - 1):
+        print(f"{index=} {a_list[index]=}")
+        location = index + n
+        result[location] = a_list[index]
 
-    for index in range(n, len(a_list)):
-        print(f"{index=}")
-        print(f"{a_list[index]=}")
-        location = index - n
-        print(f"{location=}")
+    for index in range(n - 1, len(a_list)):
+        print(f"{index=} {a_list[index]=}")
+        location = index - (n - 1)
         result[location] = a_list[index]
 
     return result
 
-if __name__ == '__main__':
-    a_list = [10, 20, 30, 40, 50]
-    a_n = 3
-    a_result = [30, 40, 50, 10, 20]
-    result = right_rotate(a_list, a_n)
-    message = f"No match. {result=}"
 
-    assert result == a_result, message
+if __name__ == '__main__':
+    import unittest
+
+    class RotateTest(unittest.TestCase):
+
+        def test_1(self):
+            arr = [1, 2, 3, 4, 5]
+            n = 1
+            expected = [5, 1, 2, 3, 4]
+            result = right_rotate(arr, n)
+            self.assertEqual(expected, result)
+
+        def test_3(self):
+            arr = [300, -1, 3, 0]
+            n = 1
+            expected = [0, 300, -1, 3]
+            result = right_rotate(arr, n)
+            self.assertEqual(expected, result)
+
+        def test_1(self):
+            arr = [0, 0, 0, 2]
+            n = 1
+            expected = [2, 0, 0, 0]
+            result = right_rotate(arr, n)
+            self.assertEqual(expected, result)
+
+        def test_1(self):
+            arr = ['13', 'a', 'Python']
+            n = 1
+            expected = ['Python', '13', 'a']
+            result = right_rotate(arr, n)
+            self.assertEqual(expected, result)
+
+        def test_1(self):
+            arr = [10, 20, 30, 40, 50]
+            n = 3
+            expected = [30, 40, 50, 10, 20]
+            result = right_rotate(arr, n)
+            self.assertEqual(expected, result)
+
+    unittest.main()
 
