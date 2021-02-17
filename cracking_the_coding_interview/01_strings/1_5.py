e28be8455f75af9761ec4dd1b6578a81a59cc788
daniel <danielkaczmarczyk2@gmail.com>
2021-02-16 20:49:10 -0800

    wip 1_5

--- cracking_the_coding_interview/01_strings/1_5.py
+++ cracking_the_coding_interview/01_strings/1_5.py
@@ -4,9 +4,17 @@ Cracking the Coding Interview, question 1.5, Page 91
 there are three types of edits that can be performed on a string:
 insert, remove, or replace a character
 
+the function should check if those strings are one edit away
+
 hints 23 97 130
 """
 def one_away(string_1, string_2):
+    # removing / inserting case
+    # if the length difference is greater than 1
+    # that means they're not one away
+    if abs(len(string_1) - len(string_2)) > 1:
+        return False
+    
     pass
 
 
@@ -26,5 +34,12 @@ if __name__ == '__main__':
             string_2 = 'bake'
 
             self.assertFalse(one_away(string_1, string_2))
+
+        def test_3(self):
+            string_1 = 'pale'
+            string_2 = 'bakery'
+
+            self.assertFalse(one_away(string_1, string_2))
+
 
     unittest.main()
