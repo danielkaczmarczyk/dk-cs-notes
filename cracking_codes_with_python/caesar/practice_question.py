48c81e946bb34efa912a8e182fc5bfb34911b9de
daniel <danielkaczmarczyk2@gmail.com>
2021-02-25 09:59:41 -0800

    added dictionary

---
+++ cracking_codes_with_python/caesar/practice_question.py
@@ -0,0 +1,12 @@
+messages = [
+    'qeFIP?eGSeECNNS',
+    '5coOMXXcoPSZIWoQI',
+    'avnl1olyD4l\'ylDohww6DhzDjhuDil',
+    'z.GM?.cEQc. 70c.7KcKMKHA9AGFK',
+    '?MFYp2pPJJUpZSIJWpRdpMFY',
+    'ZqH8sl5HtqHTH4s3lyvH5zH5spH4t pHzqHlH3l5',
+    'Zfbi,!tif!xpvme!qspcbcmz!fbu!nf'
+]
+
+for message in messages:
+    print(message)
