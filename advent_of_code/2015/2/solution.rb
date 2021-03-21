bc72ffcb541c174cfe29d2c618cad9dd4ea390bc
daniel <danielkaczmarczyk2@gmail.com>
2021-03-20 20:37:37 -0700

    working on 2

--- advent_of_code/2015/2/solution.rb
+++ advent_of_code/2015/2/solution.rb
@@ -1,22 +1,51 @@
 class Present
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
   def initialize(size_string)
-    puts size_string
+    @size_string = size_string.strip
+    self.parse_size
+    self.calculate_area
+    @@all << self
+  end
+
+  def to_s
+    "P> dim:#{@size_string} s_a:#{@size_array.to_s} area:#{@area} "
   end
 
   def self.parse_bulk(sizes_array)
     sizes_array.each do |size|
-      puts size
+      Present.new size
     end
+  end
+
+  def parse_size
+    @size_array = @size_string.strip.split('x').map { |n| n.to_i }.sort
+  end
+
+  def calculate_area
+    sa = @size_array
+    @area = 2 * ((sa[0] * sa[1]) + (sa[1] * sa[2]) + (sa[2] * sa[0]))
+    @area += self.calculate_shortest_side_area
+    @area
+  end
+
+  def calculate_shortest_side_area
+    @size_array[0] * @size_array[1]
   end
 end
 
 class Main
   def self.run
-    input = File.readlines('input_gh.txt')
-    input = input[..-2] # cut off the last empty line 
+    input = File.readlines('input_gh.txt')[..-2] # cut off the last empty line
     input = input[0..5] # cut down on input size for debugging brevity
     Present.parse_bulk input
   end
 end
 
 Main::run
+puts Present.all
+
