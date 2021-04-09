cd2c73bd79b5179f4abca374f9a2bc2f401925f3
daniel <danielkaczmarczyk2@gmail.com>
2021-04-09 12:51:31 -0700

    solved 2015.4.2.rb

--- advent_of_code/2015/4/2015_4_1.rb
+++ advent_of_code/2015/4/2015_4_1.rb
@@ -6,7 +6,8 @@ while true
   hash = Digest::MD5.new
   hash.update "#{input}#{n.to_s}"
   puts hash.to_s
-  if hash.to_s.start_with? '00000'
+  # the only difference between pt 1 and pt2 is the number of zeroes
+  if hash.to_s.start_with? '000000'
     puts "We found n: #{n}"
     break
   end
