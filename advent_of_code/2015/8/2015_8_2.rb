b3d6034f3a61043afc738596a6f8ff6d19e00047
daniel <danielkaczmarczyk2@gmail.com>
2021-04-12 11:56:49 -0700

    solved 2015.8.2

---
+++ advent_of_code/2015/8/2015_8_2.rb
@@ -0,0 +1,28 @@
+# v.0.0.1 - just works, no improvements or optimizations.
+
+filename = './input.txt'
+data = File.read filename
+
+raw_count = 0
+
+data.each_char do |char|
+  if char == "\n"
+  else
+    raw_count += 1
+  end
+end
+
+puts "raw_count: #{raw_count}"
+
+data_by_lines = File.readlines filename
+data_by_lines = data_by_lines[0..-2]
+
+normal_count = 0
+data_by_lines.each do |line|
+  size = line.strip.inspect.size
+  normal_count += size
+end
+
+puts "normal_count: #{normal_count}"
+diff = normal_count - raw_count 
+puts "result: #{diff}"
