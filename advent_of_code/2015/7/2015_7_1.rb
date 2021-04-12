8ed4a061934b2385d3864a839cfdcacf1a4bf46d
daniel <danielkaczmarczyk2@gmail.com>
2021-04-11 21:39:37 -0700

    still optimising the lookup algorithm

--- advent_of_code/2015/7/2015_7_1.rb
+++ advent_of_code/2015/7/2015_7_1.rb
@@ -1,6 +1,6 @@
-# 1. create a hash with the 'definitions'
+require 'byebug'
 
-input = File.readlines('./test_input.txt')
+input = File.readlines('./input.txt')
 $answers = File.readlines('./test_output.txt')
 
 $wires = {}
@@ -9,16 +9,6 @@ input.each do |line|
   wire_data = line.split('->').map { |str| str.strip }
   $wires[wire_data[1]] = wire_data[0]
 end
-
-
-
-#2 resolve a value by jumping around the hash
-
-# if it resolves to a integer, that's your value.
-# if it doesn't resolve to an integer, it means it's not yet fully resolved
-# it may have 2-3 tokens to be parsed
-  # if it's 2, then it's a binary operation and we perform it and we're done
-  # if it's 3, we provide the _same_ method of resolution we did in the original mode - just call upon this recursively
 
 def remap(operation)
   # p "input to remap: #{operation}"
@@ -31,53 +21,36 @@ def remap(operation)
   ops[operation.to_sym]
 end
 
-def resolve(string)
-  # puts "input: #{string}"
+$count = 0
+def resolve(string, previous=nil)
+  throw "the input was NIL buddy" if string.nil?
+
+  $count += 1
+  print "___#{$count} "
+  puts "input to resolve: #{string}"
   split_string = string.split(' ')
   size = split_string.size
-
   if size == 1
     begin
-      Integer(string)
+      puts "Trying to make an integer from #{string}"
+      number = Integer(string)
     rescue
-      # puts 'in the rescue clause'
-      # puts "$wires:[string]: #{$wires[string]}"
-      return Integer($wires[string])
+      puts "Failed making an integer from #{string}"
+      num = resolve($wires[string], string)
+      $wires[string] = num
+      return num
     end
   elsif size == 2
-    # that's the NOT case
-    #puts "in the size==2 case. split_string:#{split_string}"
-    #return ~resolve(split_string[1])
-    return 65535 - resolve(split_string[1])
+    num = 65535 - resolve(split_string[1])
+    $wires[previous] = num
+    return num
   elsif size == 3
-    # that's the <VAR> <OPERAND> <VAR> case.
-    # basically I need to make some recursive calls and when they both
-    # resolve, I need to perform the actual operation.
-    #p "#{resolve(split_string[0])} #{remap(split_string[1])} #{resolve(split_string[2])}"
-    return eval "#{resolve(split_string[0])} #{remap(split_string[1])} #{resolve(split_string[2])}"
+    num = eval "#{resolve(split_string[0])} #{remap(split_string[1])} #{resolve(split_string[2])}"
+    puts "writing #{num} to $wires[#{previous}]"
+    $wires[previous] = num
+    return num
   end
 end
 
-def test_input_cases
-  pp $wires
-  pp $answers
-  puts ""
+p resolve 'a'
 
-  print('d: ')
-  p resolve $wires['d']
-  print('e: ')
-  p resolve $wires['e']
-  print('f: ')
-  p resolve $wires['f']
-  print('g: ')
-  p resolve $wires['g']
-  print('h: ')
-  p resolve $wires['h']
-  print('i: ')
-  p resolve $wires['i']
-  print('x: ')
-  p resolve $wires['x']
-  print('y: ')
-  p resolve $wires['y']
-end
-#test_input_cases
