bb5dc8297892260cbd5bcc8b8e03a17362d28679
daniel <danielkaczmarczyk2@gmail.com>
2021-04-13 13:48:56 -0700

    implemented a simple bfs

--- grokking_algorithms/ch6/mangoes.rb
+++ grokking_algorithms/ch6/mangoes.rb
@@ -1,12 +1,19 @@
-graph = {}
+# what would happen if there was no mango sellers?
+# - the queue ends up empty eventually
+# - as long as there are no circular references, we end our search
+# what would happen if there was no mango sellers and we would have a circular reference?
+# - the search would continue into infinity
+# - to alleviate this problem we need to keep track of people we've already seen
 
+# declaring the graph by hand
+graph = {}
 graph['you'] = ['bob', 'claire', 'alice']
 graph['bob'] = ['anuj', 'peggy']
-graph['claire'] = ['thom', 'jonny']
+graph['claire'] = ['tho', 'jonny']
 graph['alice'] = ['peggy']
 graph['anuj'] = []
-graph['peggy'] = []
-graph['thom'] = []
+graph['peggy'] = ['bob']
+graph['tho'] = []
 graph['jonny'] = []
 
 
@@ -22,24 +29,29 @@ def sells_mangoes?(person)
   person[-1] == 'm'
 end
 
-queue = []
+def bfs(graph)
+  queue = []
+  queue += graph['you']
+  searched = []
 
-queue += graph['you']
-
-while !queue.empty?
-  pp queue
-  current_person = queue.shift
-  puts "looking at #{current_person}"
-  if sells_mangoes? current_person
-    puts "we found the mango seller!: #{current_person}"
-    return
-  else
-    puts "#{current_person} is not selling mangoes. adding their friends to the q"
-    queue += graph[current_person]
+  while !queue.empty?
+    puts "q: #{queue}"
+    puts "s: #{searched}"
+    current_person = queue.shift
+    puts "looking at #{current_person}"
+    if not (searched.include? current_person)
+      if sells_mangoes? current_person
+        puts "we found the mango seller!: #{current_person}"
+        return
+      else
+        puts "#{current_person} is not selling mangoes. adding their friends to the q"
+        queue += graph[current_person]
+        searched << current_person
+      end
+    else
+      puts "we already checked if #{current_person} is a mango seller. that person leaves the queue"
+    end
   end
+  puts "no mango seller found."
 end
-
-
-# what would happen if there was no mango sellers?
-
-# what would happen if there was no mango sellers and we would have a circular reference?
+bfs graph
