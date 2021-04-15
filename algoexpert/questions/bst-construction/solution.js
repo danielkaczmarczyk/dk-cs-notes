4d5f0c359fb7258cb69b8de79f2bf8ea3bfd0673
daniel <danielkaczmarczyk2@gmail.com>
2021-04-14 20:07:02 -0700

    made some progress on the bst remove

--- algoexpert/questions/bst-construction/solution.js
+++ algoexpert/questions/bst-construction/solution.js
@@ -69,14 +69,68 @@ class BST {
     }
   }
 
-  remove(value) {
-    //
+  remove(value, parentNode = null) {
+    let currentNode = this;
+
+    // removal is a 2- step process. first find the node you want to remove
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        // the main branch of deletion -> it means we've found the node we are to delete!
+        // CASE: node that has 2 children nodes
+        // find the smallest val of the right subtree
+        // replace it with the value that we're removing
+        if (currentNode.left !== null && currentNode.right !== null) {
+          currentNode.value = currentNode.right.getMinValue();
+          // we've set currentNode.value to be the smallest value
+          // now we want to remove the node whose value we snatched
+          // important - we're passing the parent node!
+          currentNode.right.remove(currentNode.value, currentNode);
+          // we are gonna come back to the root node case
+        } else if (parentNode === null) {
+          // we already dealt with a case where the node has two kids so we gon'
+          // just do left and right here
+          if (currentNode.left !== null) {
+            currentNode.value = currentNode.left.value;
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+          } else if (currentNode.right !== null) {
+            currentNode.value = currentNode.right.value;
+            currentNode.left = currentNode.right.left;
+            currentNode.right = currentNode.right.right;
+          } else {
+            // the root node has no parent nodes and has no children
+            // more of a mock case - could be disregarded
+            currentNode.value = null;
+          }
+        } else if (parentNode.left == currentNode) {
+          // that node is a left child of its parent node
+          // therefore the parent node.left is gonna be reassigned to either
+          // the left child node of our current node, if it has one
+          // otherwise, the rc n
+          if (currentNode.left !== null) {
+            parentNode.left = currentNode.left;
+          } else {
+            parentNode.left = currentNode.right;
+          }
+        } else if (parentNode.right == currentNode) {
+          if (currentNode.left !== null) {
+            parentNode.right = currentNode.left;
+          } else {
+            parentNode.right = currentNode.right;
+          }
+        }
+      }
+      break;
+    }
+    return this;
   }
 }
 
 tree = new BST(10);
 [5, 15, 2, 5, 1, 13, 22, 14].forEach((n) => tree.insert(n));
-log(tree.getMinValue());
-log(tree.getMaxValue());
-//console.log(util.inspect(tree, { depth: null }));
-//console.log(tree.contains(124));
