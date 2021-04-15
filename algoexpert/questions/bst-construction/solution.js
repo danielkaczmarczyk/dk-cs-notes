81a4874675a51f1df823d3dd2258f2556150580b
daniel <danielkaczmarczyk2@gmail.com>
2021-04-14 18:59:11 -0700

    removed some comments

--- algoexpert/questions/bst-construction/solution.js
+++ algoexpert/questions/bst-construction/solution.js
@@ -8,21 +8,16 @@ class BST {
   }
 
   insert(value) {
-    // we start at the root node.
-    // it has two children, and both are null.
     let currentNode = this;
     while (true) {
       if (value < currentNode.value) {
-        // go left in the tree
         if (currentNode.left) {
           currentNode = currentNode.left;
         } else {
-          // there is no left child.
           currentNode.left = new BST(value);
           break;
         }
       } else {
-        // the value is >= currentNode.value, therefore we are going to be moving right
         if (currentNode.right) {
           currentNode = currentNode.right;
         } else {
@@ -35,15 +30,11 @@ class BST {
   }
 
   contains(value) {
-    // keep searching, similarly to insertion, until we find a value.
     let currentNode = this;
 
-    // searching !
     while (true) {
-      // check if we found it
       if (currentNode.value === value) return true;
 
-      // if we did not find the node, we need to move.
       if (currentNode.value > value) {
         if (currentNode.left) {
           currentNode = currentNode.left;
