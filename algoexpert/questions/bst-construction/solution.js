d76e5b2d68bc76267d9506e79bdd62e3fb829914
daniel <danielkaczmarczyk2@gmail.com>
2021-04-14 18:48:10 -0700

    solved contains for bst

--- algoexpert/questions/bst-construction/solution.js
+++ algoexpert/questions/bst-construction/solution.js
@@ -33,8 +33,35 @@ class BST {
     }
     return this;
   }
+
+  contains(value) {
+    // keep searching, similarly to insertion, until we find a value.
+    let currentNode = this;
+
+    // searching !
+    while (true) {
+      // check if we found it
+      if (currentNode.value === value) return true;
+
+      // if we did not find the node, we need to move.
+      if (currentNode.value > value) {
+        if (currentNode.left) {
+          currentNode = currentNode.left;
+        } else {
+          return false;
+        }
+      } else if (currentNode.value < value) {
+        if (currentNode.right) {
+          currentNode = currentNode.right;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
 }
 
 tree = new BST(10);
 [5, 15, 2, 5, 1, 13, 22, 14].forEach((n) => tree.insert(n));
-console.log(util.inspect(tree, { depth: null }));
+//console.log(util.inspect(tree, { depth: null }));
+//console.log(tree.contains(124));
