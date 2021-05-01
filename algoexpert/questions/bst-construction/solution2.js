e0344f53712bfa8d92aade669aed4d49f9cac016
daniel <danielkaczmarczyk2@gmail.com>
2021-05-01 09:48:04 -0700

    just deletion left!

--- algoexpert/questions/bst-construction/solution2.js
+++ algoexpert/questions/bst-construction/solution2.js
@@ -27,9 +27,32 @@ class BST {
       }
     }
   }
+
+  contains(value) {
+    let currentNode = this;
+
+    while (true) {
+      if (currentNode.value === value) return true;
+
+      if (value < currentNode.value) {
+        if (currentNode.left) {
+          currentNode = currentNode.left;
+        } else {
+          return false;
+        }
+      } else {
+        if (currentNode.right) {
+          currentNode = currentNode.right;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
 }
 
 const tree = new BST(10);
 [5, 15, 2, 5, 1, 13, 22, 14, 12].forEach((n) => tree.insert(n));
 
 console.log(util.inspect(tree, false, null, true));
+console.log(tree.contains(2));
