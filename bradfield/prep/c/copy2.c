b9bf8b24eb63737a6f57ef1bf76907e6e6d377b7
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2021-08-06 20:19:44 -0700

    amend gitignore

---
+++ bradfield/prep/c/copy2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int c;
+
+  while ((c = getchar()) != EOF)
+  {
+    putchar(c);
+  }
+}
---
+++ bradfield/prep/c/eofval.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("the value of EOF: %d\n", EOF);
+}
