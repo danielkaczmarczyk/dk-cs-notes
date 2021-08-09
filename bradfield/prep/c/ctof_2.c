094f1555eddd3f47cb2b09a2d3d0478d86e6c919
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2021-08-08 20:52:21 -0700

    solved thru 1-15

--- bradfield/prep/c/ctof_2.c
+++ bradfield/prep/c/ctof_2.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
+#define LOWER_BOUNDARY 0
+#define UPPER_BOUNDARY 300
+#define STEP 20
+
 /* print a c to f conversion table */
+
+int c_to_f(float cel_temp);
+void print_temp(float cel_temp, float fah_temp);
 
 int main(void) {
   float cel_temp, fah_temp;
-  int lower_boundary, upper_boundary, step;
 
-  lower_boundary = 0;
-  upper_boundary = 300;
-  step = 20;
-
-  for (cel_temp = upper_boundary; cel_temp >= lower_boundary; cel_temp -= step) {
-    fah_temp = ( cel_temp * 9 / 5 ) + 32;
-    printf("C: %-6.2f F: %-6.2f\n", cel_temp, fah_temp);
+  for (cel_temp = UPPER_BOUNDARY; cel_temp >= LOWER_BOUNDARY; cel_temp -= STEP) {
+    fah_temp = c_to_f(cel_temp);
+    print_temp(cel_temp, fah_temp);
   }
 }
 
+int c_to_f(float cel_temp) {
+  return (cel_temp * 9 / 5) + 32;
+}
+
+void print_temp(float cel_temp, float fah_temp) {
+  printf("C: %-6.2f F: %-6.2f\n", cel_temp, fah_temp);
+}
