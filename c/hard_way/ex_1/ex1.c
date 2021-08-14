66ee7bef8662a7a148c0bfb361cd69f2738d80ba
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2021-08-14 11:28:48 -0700

    more c exercises

---
+++ c/hard_way/ex_1/ex1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("This is a test file. Gonna use a makefie to make it\n");
+}
---
+++ c/hard_way/ex_3/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int age;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+    return 0;
+}
+
