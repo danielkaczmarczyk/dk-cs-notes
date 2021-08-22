8e1b07cc94eb027408d129e2fdcb7eef8636be55
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2021-08-21 22:49:19 -0700

    more go notes

---
+++ go/ch_2/cf.go
@@ -0,0 +1,20 @@
+// Cf converts its numeric argument to Celsius and Fahrenheit
+package main
+
+import (
+    "fmt"
+    "os"
+    "strconv"
+
+    "gopl.io/ch2/tempconv"
+)
+
+func main() {
+    for _, arg := range os.Args[1:] {
+        t, err := strconv.ParseFloat(arg, 64)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+        }
+    }
+}
+
---
+++ go/ch_2/go.mod
@@ -0,0 +1,5 @@
+module example.com/ch_2
+
+go 1.16
+
+require gopl.io v0.0.0-20200323155855-65c318dde95e
