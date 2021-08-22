62f4c94f39801f5715a5feff94cf3a4a1a12b882
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2021-08-21 20:28:34 -0700

    echo4

---
+++ go/ch_2/echo4.go
@@ -0,0 +1,19 @@
+// echo4 prints its cl args
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
