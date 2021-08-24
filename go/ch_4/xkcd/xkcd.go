0748cded267eac5b9066568976417da0b6ce722e
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2021-08-24 09:41:39 -0700

    decoding json

--- go/ch_4/xkcd/xkcd.go
+++ go/ch_4/xkcd/xkcd.go
@@ -2,11 +2,64 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
+
+type Comics struct {
+    Comics []Comic `json:"comics"`
+}
+
+type Comic struct {
+    Day        int `json:",string"`
+	Month      string 
+    Num        int `json:"num"`
+	Year       string
+	News       string
+	Title      string
+	SafeTitle  string
+	Transcript string
+	Img        string
+	Alt        string
+}
+
+func printComic(comic Comic) {
+    fmt.Println("num: ", comic.Num)
+    fmt.Println("day: ", comic.Day)
+    fmt.Println("month: ", comic.Month)
+    fmt.Println("year: ", comic.Year)
+    fmt.Println("news: ", comic.News)
+    fmt.Println("title: ", comic.Title)
+    fmt.Println("safeTitle: ", comic.SafeTitle)
+    fmt.Println("transcript: ", comic.Transcript)
+    fmt.Println("img: ", comic.Img)
+    fmt.Println("alt: ", comic.Alt)
+    fmt.Println("-----------------------")
+}
+
+func loadJSON() {
+	// read the json file into memory
+    jsonFile, err := os.Open("xkcd.json")
+    check(err)
+    fmt.Println("successfully opened xkcd.json")
+    defer jsonFile.Close()
+
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+    var comics Comics
+
+	// parse in as an array of Comic structs
+    json.Unmarshal(byteValue, &comics)
+   
+    for _, comic := range comics.Comics {
+        printComic(comic)
+    }
+
+	// declare a global var with the necessary size
+     
+}
 
 func getURL(comicId int) (url string) {
 	return "https://xkcd.com/" + fmt.Sprint(comicId) + "/info.0.json"
@@ -18,7 +71,8 @@ func check(err error) {
 	}
 }
 
-func main() {
+func downloadAll() {
+
 	var comicId int = 1
 	f, err := os.Create("./xkcd.json")
 	check(err)
@@ -59,6 +113,10 @@ func main() {
 
 	w.WriteString("]}\n")
 	w.Flush()
+}
+
+func main() {
+    loadJSON()
 }
 
 // TODO
