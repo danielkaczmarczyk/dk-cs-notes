ec8bfdb0f396d39f35ad7d755ccd50814a879e6f
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2021-10-30 23:49:38 -0700

    asdf

---
+++ go/go_programming_language/ch_6/geo/go.mod
@@ -0,0 +1,3 @@
+module example.com/e
+
+go 1.16
---
+++ go/go_programming_language/ch_6/geo/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct{ X, Y float64 }
+
+// traditional function
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// same thing, but as a method of the Point type
+//    v this extra parameter 'p' is called the receiver
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+type Path []Point
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
+func (p *Point) ScaleBy(factor float64) {
+  p.X *= factor
+  p.Y *= factor
+}
+
+func main() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	fmt.Println(Distance(p, q))
+	fmt.Println(p.Distance(q))
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+	fmt.Println(perim.Distance())
+}
